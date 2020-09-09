48138c13f212157e2b35ffeb9a51d3a3bb96c0dd
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-09-09 17:41:34 +0530

    Spawnner Script adjustment

--- Assets/Scripts/Spawnner.cs
+++ Assets/Scripts/Spawnner.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Spawnner : MonoBehaviour
 {
     public spawn[] block;
-    [SerializeField] float spawnDelay = 3f;
+    [SerializeField] float spawnDelay;
     [SerializeField] float DelayUntilRateDecrease = 30f;
+    [SerializeField] float timeBeforeSpawnStart;
+
+    Vector2 screenHalfSizeWorlUnits;
     void Start()
     {
-        StartCoroutine(SpawnObjects(spawnDelay));   
+        screenHalfSizeWorlUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize + 5f, Camera.main.orthographicSize);
+        spawnDelay = Random.Range(2.5f, 6f);
+        float tempRandom = Random.Range(0.5f, 1.5f);
+        spawnDelay += tempRandom;
+        timeBeforeSpawnStart = Random.Range(1f, 5f);
+        StartCoroutine(SpawnObjects(spawnDelay));
     }
 
 
@@ -17,36 +25,47 @@ public class Spawnner : MonoBehaviour
     {
         float spawnRateCountDown = DelayUntilRateDecrease;
         float spawnCountDown = firstDelay;
+        float intialSpawnDelay = timeBeforeSpawnStart;
 
         while(true)
         {
             yield return null;
-
             spawnRateCountDown -= Time.deltaTime;
             spawnCountDown -= Time.deltaTime;
+            intialSpawnDelay -= Time.deltaTime;
+            //Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorlUnits.x, screenHalfSizeWorlUnits.x), screenHalfSizeWorlUnits.y);
 
             // Should a new object be spawnned?
-            if(spawnCountDown < 0)
+            if(intialSpawnDelay< 0)
             {
-                spawnCountDown += spawnDelay;
-                int i = Random.Range(0, 100);
-
-                for (int j = 0; j < block.Length; j++)
+                if(spawnCountDown < 0)
                 {
-                    if (i >= block[j].minProbabilityRange && i <= block[j].maxProbabilityRange)
-                    {
-                        Instantiate(block[j].spawnObject, transform.position, Quaternion.identity);
-                        break;
-                    }
+                    spawnCountDown += spawnDelay;
+                    InitiateSpawn();
                 }
             }
-
-            if(spawnRateCountDown < 0  && spawnDelay > 1)
+            
+            // Should the rate decrease?
+            if (spawnRateCountDown < 0  && spawnDelay > 1)
             {
                 spawnRateCountDown += DelayUntilRateDecrease;
                 spawnDelay -= 0.2f;
             }
 
+        }
+    }
+
+    private void InitiateSpawn()
+    {
+        int i = Random.Range(0, 100);
+
+        for (int j = 0; j < block.Length; j++)
+        {
+            if (i >= block[j].minProbabilityRange && i <= block[j].maxProbabilityRange)
+            {
+                Instantiate(block[j].spawnObject, transform.position, Quaternion.identity);
+                break;
+            }
         }
     }
 }
